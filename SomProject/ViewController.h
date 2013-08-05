e8c2da9dced5939619419575dbbe5721d35849d9
Sukwon Choi <swchoi06@hanmail.net>
2013-08-05 18:42:32 +0900

    Initial Commit

---
+++ SomProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SomProject
+//
+//  Created by SukWon Choi on 13. 8. 5..
+//  Copyright (c) 2013년 Wafflestudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SomProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SomProject
+//
+//  Created by SukWon Choi on 13. 8. 5..
+//  Copyright (c) 2013년 Wafflestudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
